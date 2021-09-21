5f4de24fd6ea8616cede8ad06ca38ca77f15db30
LattanzioB <brunoj.lattanzio@gmail.com>
2021-09-21 16:49:27 -0300

    base entrega-2

---
+++ entrega-2/entrega-2-lattanzio-bruno/entrega-2-lattanzio-bruno/entities/player/Player.gd
@@ -0,0 +1,32 @@
+extends Sprite
+
+
+onready var cannon:Sprite = $Cannon
+
+export (float) var ACCELERATION:float = 20.0
+export (float) var H_SPEED_LIMIT:float =600.0
+export (float) var FRICTION_WEIGHT:float = 0.1
+
+var velocity:Vector2 = Vector2.ZERO
+var projectile_container:Node
+
+func set_projectile_container(container:Node):
+	cannon.projectile_container = container
+	projectile_container = container
+
+func _physics_process(delta):
+
+	var mouse_postion:Vector2 = get_global_mouse_position()
+	cannon.look_at(mouse_postion)
+	
+	if Input.is_action_just_pressed("fire"):
+		cannon.fire()
+	
+	var h_movement_direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	if h_movement_direction != 0:
+		velocity.x = clamp(velocity.x + (h_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+	else:
+		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
+		
+	position += velocity * delta
